ccc2da44178c549c0f2f842f4a07a0e980618e3c
MagShadowLight <williamrose1025@icloud.com>
2023-09-19 18:24:06 -0700

    Initial commit

---
+++ src/main/java/death/DeathCounterClient.java
@@ -0,0 +1,10 @@
+package death;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeathCounterClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
