966bfbc2cfe0266c5ea0c04c1469966286c15322
MagShadowLight <williamrose1025@icloud.com>
2023-09-22 19:59:53 -0700

    stuff

--- src/main/java/death/mixin/ExampleMixin.java
+++ src/main/java/death/mixin/ExampleMixin.java
@@ -5,14 +5,17 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "onDeath")
+public abstract class ExampleMixin {
+
+
+	@Inject(at = @At("TAIL"), method = "onDeath")
 	private void init(CallbackInfo info) {
-		MinecraftClient.getInstance().player.sendMessage(Text.literal("Owwwww"));
+
 	}
 }
