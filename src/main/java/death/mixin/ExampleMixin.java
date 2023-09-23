11502f898265db193e8d26ae50c2e223dab44b24
MagShadowLight <williamrose1025@icloud.com>
2023-09-22 19:15:07 -0700

    need help

--- src/main/java/death/mixin/ExampleMixin.java
+++ src/main/java/death/mixin/ExampleMixin.java
@@ -2,16 +2,16 @@ package death.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 
-@Mixin(MinecraftServer.class)
+@Mixin(PlayerEntity.class)
 public class ExampleMixin {
 	@Inject(at = @At("TAIL"), method = "onDeath")
-	private void death(DamageSource damage) {
-		MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.literal("Owwwwww"));
+	public void death(DamageSource damage) {
+
 	}
 }
